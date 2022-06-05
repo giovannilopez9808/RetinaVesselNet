1a04f3bd96d7a44816411b468cf530322c70677a
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-06-04 20:51:03 -0500

    Add grayscale image

--- Scripts/make_high_contrast_images.py
+++ Scripts/make_high_contrast_images.py
@@ -1,10 +1,12 @@
 from os import listdir as ls
 from os.path import join
+from os import makedirs
+from numpy import array
 from tqdm import tqdm
 import cv2
 
 
-def high_contrast_image(img):
+def high_contrast_image(img: array) -> array:
     """
     Filtro de alto contraste a la imagen
     """
@@ -26,8 +28,22 @@ def high_contrast_image(img):
     return img
 
 
+def to_grayscale(img: array) -> array:
+    grayscale = cv2.cvtColor(img,
+                             cv2.COLOR_BGR2GRAY)
+    return grayscale
+
+
+def mkdir(path: str) -> None:
+    makedirs(path,
+             exist_ok=True)
+
+
 params = {
     "path data": "../Data",
+    "path images": "normal",
+    "path grayscale": "grayscale",
+    "path high contrast": "high_contrast",
     "folders": [
         "test",
         "validate",
@@ -38,16 +54,29 @@ params = {
 for folder in params["folders"]:
     path = join(params["path data"],
                 folder,
-                "image/data")
-    path_out = join(params["path data"],
-                    folder,
-                    "high_contrast/data")
+                params["path images"],
+                "data")
+    path_high_contrast = join(params["path data"],
+                              folder,
+                              params["path high contrast"],
+                              "data")
+    path_grayscale = join(params["path data"],
+                          folder,
+                          params["path grayscale"],
+                          "data")
+    mkdir(path_high_contrast)
+    mkdir(path_grayscale)
     files = sorted(ls(path))
-    for file in tqdm(files):
+    for file in tqdm(files,
+                     desc=f"{folder} images"):
         filename = join(path, file)
         image_original = cv2.imread(filename)
         image = high_contrast_image(image_original)
-        filename = join(path_out,
+        grayscale = to_grayscale(image)
+        filename = join(path_high_contrast,
+                        file)
+        cv2.imwrite(filename, image)
+        filename = join(path_grayscale,
                         file)
         cv2.imwrite(filename,
-                    image)
+                    grayscale)
